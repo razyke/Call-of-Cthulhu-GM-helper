2bf424e2af44cd173af3237c3905dd0deee2b29c
Orbot90 <orbot90@gmail.com>
2018-11-27 00:39:33 +0300

    Added bootstrap and a template for the first component to ui part

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiceRollComponent } from './components/diceroll/dice-roll.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: DiceRollComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
